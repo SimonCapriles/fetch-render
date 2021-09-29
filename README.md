4fb8f3ae2520aea559e3782dc658d8a014d37214
Simon Capriles <simon.capriles@kyohei.com.bo>
2021-09-29 17:53:13 -0400

    Fetch and render project created

--- README.md
+++ README.md
@@ -1,70 +1,29 @@
-# Getting Started with Create React App
+# Fetch and Render
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+> Version 1.0.0.1
 
-## Available Scripts
+A project that retrieves data from an API and renders a list of pages.
 
-In the project directory, you can run:
+![fetch-render.gif](./public/fetch-render.gif)
 
-### `npm start`
+What is being **used** in this project:
+- **React** to create the whole project
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## What it does
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+1. Retrieves data from a given **API**. 
+2. The returned data is **Rendered** in a list.
 
-### `npm test`
+## Maintainers
+Who worked on this:
+- [Simon Capriles](https://simoncapriles.github.io/)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Support
 
-### `npm run build`
+Found some issues?  
+Write to this email: <a href="mailto:info@kyohei.com.bo"><i class="font-icon icon-envelope"></i>info@kyohei.com.bo</a>
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Github
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Want to download or fork the project?  
+Find it on Github: [submit-form](https://github.com/SimonCapriles/submit-form)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,154 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+
+const Pagination = ({ items, pageSize, onPageChange }) => {
+    if (items.length <= 1) return null;
+
+    let num = Math.ceil(items.length / pageSize);
+    let pages = range(1, num + 1);
+    const list = pages.map(page => {
+        return (
+            <Button key={page} onClick={onPageChange} className="page-item">
+                {page}
+            </Button>
+        );
+    });
+    return (
+        <nav>
+            <ul className="pagination">{list}</ul>
+        </nav>
+    );
+};
+
+const range = (start, end) => {
+    return Array(end - start + 1)
+        .fill(0)
+        .map((item, i) => start + i);
+};
+
+function paginate(items, pageNumber, pageSize) {
+    const start = (pageNumber - 1) * pageSize;
+    return items.slice(start, start + pageSize);
+}
+
+const useDataApi = (initialUrl, initialData) => {
+    const { useState, useEffect, useReducer } = React;
+    const [url, setUrl] = useState(initialUrl);
+
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+        isLoading: false,
+        isError: false,
+        data: initialData
+    });
+
+    useEffect(() => {
+        let didCancel = false;
+        const fetchData = async () => {
+            dispatch({ type: "FETCH_INIT" });
+            try {
+                const result = await axios(url);
+                if (!didCancel) {
+                    dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+                }
+            } catch (error) {
+                if (!didCancel) {
+                    dispatch({ type: "FETCH_FAILURE" });
+                }
+            }
+        };
+        fetchData();
+        return () => {
+            didCancel = true;
+        };
+    }, [url]);
+    console.log(`The URL is: ${url}`);
+    return [state, setUrl];
+};
+
+const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_INIT":
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            };
+        case "FETCH_SUCCESS":
+            console.log(action.payload);
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload
+            };
+        case "FETCH_FAILURE":
+            return {
+                ...state,
+                isLoading: false,
+                isError: true
+            };
+        default:
+            throw new Error();
+    }
+};
 
 function App() {
+  const { Fragment, useState } = React;
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+      "https://ghibliapi.herokuapp.com/films/",
+      {
+        hits: []
+      }
+  );
+  const handlePageChange = e => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  console.log(`The page is: ${data}`);
+  if (data.length >= 1) {
+    page = paginate(data, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Fragment>
+        <form
+            onSubmit={event => {
+              doFetch("https://ghibliapi.herokuapp.com/films");
+              event.preventDefault();
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input
+              type="text"
+              value={query}
+              onChange={event => setQuery(event.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        {isError && <div>Something went wrong ...</div>}
+
+        {isLoading ? (
+            <div>Loading ...</div>
+        ) : (
+            <ul>
+              {page.map(item => (
+                  <li key={item.id}>
+                    <a href={item.url}>{item.title}</a>
+                  </li>
+              ))}
+            </ul>
+        )}
+        <Pagination
+            items={data}
+            pageSize={pageSize}
+            onPageChange={handlePageChange}
+        />
+      </Fragment>
   );
 }
 
